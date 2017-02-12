74f5d3806527d6995fad6d6df6343be77b9d7ec2
Kata Meszaros <kata.meszaros92@gmail.com>
2017-02-12 21:15:57 +0100

    Added list highlighting, status messages and popup windows

--- story_manager/manager.py
+++ story_manager/manager.py
@@ -1,30 +1,32 @@
 import os
 from peewee import *
 from story_manager.connect_db import ConnectDatabase
-from story_manager.models import Story
+from story_manager.models import Story, Status
 from flask import Flask, request, session, g, redirect, url_for, abort, \
     render_template, flash, current_app
-
 
 app = Flask(__name__)  # create the application instance :)
 app.config.from_object(__name__)  # load config from this file , flaskr.py
 
 # Load default config and override config from an environment variable
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+app.secret_key = 'some_secret'
 
 
 def init_db():
     db = ConnectDatabase.db
     db.connect()
-    db.create_tables([Story], safe=True)
+    db.drop_table(Story, fail_silently=True, cascade=True)
+    db.drop_table(Status, fail_silently=True, cascade=True)
+    db.create_tables([Status, Story], safe=True)
 
 
 @app.cli.command('initdb')
 def initdb_command():
     """Initializes the database."""
     init_db()
+    Status.create_defaults()
     print('Initialized the database.')
-
 
 @app.teardown_appcontext
 def close_db(error):
@@ -41,7 +43,7 @@ def show_stories():
 
 @app.route('/story', methods=['GET'])
 def show_create_story_form():
-    return render_template('create.html', story=Story())
+    return render_template('create.html', story=Story.empty(), statuses=Status.select())
 
 
 @app.route('/story', methods=['POST'])
@@ -54,13 +56,14 @@ def create_story():
                              status=request.form['status']
                              )
     new_story.save()
+    flash('Story was successfully created')
     return redirect("/")
 
 
 @app.route('/story/<story_id>', methods=['GET'])
 def show_update_story_form(story_id):
     story = Story.select().where(Story.id == story_id).get()
-    return render_template('update.html', story=story)
+    return render_template('update.html', story=story, statuses=Status.select())
 
 
 @app.route('/story/<story_id>', methods=['POST'])
@@ -72,10 +75,12 @@ def update_story(story_id):
                  estimation=request.form['estimation'],
                  status=request.form['status'])\
         .where(Story.id == story_id).execute()
+    flash('Story was successfully updated')
     return redirect("/")
 
 
 @app.route('/story/delete/<story_id>', methods=['GET'])
 def delete_story(story_id):
     Story.delete().where(Story.id == story_id).execute()
+    flash('Story was successfully deleted')
     return redirect("/")
--- story_manager/models.py
+++ story_manager/models.py
@@ -1,13 +1,41 @@
 from peewee import *
 from story_manager.connect_db import ConnectDatabase
 
-class Story(Model):
-    title = CharField(default="")
-    user_story = CharField(default="")
-    acceptance_criteria = CharField(default="")
-    business_value = CharField(default="")
-    estimation = CharField(default="")
-    status = CharField(default="")
+
+class BaseModel(Model):
 
     class Meta:
         database = ConnectDatabase.db
+
+
+class Status(BaseModel):
+    PLANNING = "Planning"
+    TO_DO = "To do"
+    IN_PROGRESS = "In progress"
+    REVIEW = "Review"
+    DONE = "Done"
+
+    status_text = CharField(default="")
+
+    @classmethod
+    def create_defaults(cls):
+        Status(status_text=Status.PLANNING).save()
+        Status(status_text=Status.TO_DO).save()
+        Status(status_text=Status.IN_PROGRESS).save()
+        Status(status_text=Status.REVIEW).save()
+        Status(status_text=Status.DONE).save()
+
+class Story(BaseModel):
+    title = CharField(default="")
+    user_story = CharField(default="",max_length=10000)
+    acceptance_criteria = CharField(default="",max_length=10000)
+    business_value = CharField(default="")
+    estimation = CharField(default="")
+    status = ForeignKeyField(Status)
+
+    @classmethod
+    def empty(cls):
+        return Story(status=Status.select().where(Status.status_text == Status.PLANNING))
+
+
+
